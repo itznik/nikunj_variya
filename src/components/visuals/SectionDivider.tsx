2e26c364fdeeac436cb70847699b68374c832fc3
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-05-31 13:27:46 +0530

    Create SectionDivider.tsx

---
+++ src/components/visuals/SectionDivider.tsx
@@ -0,0 +1,7 @@
+export default function SectionDivider() {
+  return (
+    <div className="mx-auto max-w-7xl px-6 md:px-12 lg:px-20">
+      <div className="h-px w-full bg-black/6" />
+    </div>
+  );
+}
