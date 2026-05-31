a19157a8640dfceca56a84fd3a0c2a400896dc60
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-05-31 13:25:59 +0530

    Create NoiseTexture.tsx

---
+++ src/components/visuals/NoiseTexture.tsx
@@ -0,0 +1,17 @@
+export default function NoiseTexture() {
+  return (
+    <div
+      className="
+        pointer-events-none
+        fixed
+        inset-0
+        z-[1]
+        opacity-[0.03]
+      "
+      style={{
+        backgroundImage:
+          "url('https://grainy-gradients.vercel.app/noise.svg')",
+      }}
+    />
+  );
+}
