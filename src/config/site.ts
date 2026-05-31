6987e39ab902b710fce01aaae6c842b3616878b3
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-05-31 12:50:48 +0530

    Create site.ts

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "Nikunj Variya",
+
+  email: "nikvrya@gmail.com",
+
+  github: "https://github.com/yourusername",
+
+  linkedin:
+    "https://linkedin.com/in/yourusername",
+};
