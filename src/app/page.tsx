da4185007701e79688f3b2b83de9ebf6b37eb6d6
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-06-01 14:20:25 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Work from "@/components/Work";
 import Skills from "@/components/Skills";
-
+import About from "@/components/About";
 export default function Home() {
   return (
     <main className="flex flex-col w-full relative">
@@ -10,6 +10,7 @@ export default function Home() {
       <Hero />
       <Work />
       <Skills />
+      <About />
       {/* We will add Work, Skills, About, Contact here in the next steps */}
     </main>
   );
