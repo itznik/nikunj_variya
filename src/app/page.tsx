9497db81a5d0ed1a0845d00f2a97b509fe7ee9a8
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-06-01 13:18:50 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Work from "@/components/Work";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full relative">
       <Navbar />
       <Hero />
+      <Work />
       {/* We will add Work, Skills, About, Contact here in the next steps */}
     </main>
   );
