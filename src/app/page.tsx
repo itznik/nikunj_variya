80f14c47505d2cbc7f1971b9ee63359627e40b30
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-06-01 13:33:17 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Work from "@/components/Work";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Work />
+      <Skills />
       {/* We will add Work, Skills, About, Contact here in the next steps */}
     </main>
   );
